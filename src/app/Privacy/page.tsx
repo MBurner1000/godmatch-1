3127c1fb72e6d27c8b37d40d8a3bdb5494cb3959
clover00000011 <fclover996@gmail.com>
2024-02-26 13:32:05 -0800

    first commit

---
+++ src/app/Privacy/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+import MainHeader from "../ui/header";
+
+export default function PrivacyPolicy() {
+  return (
+    <main>
+      <MainHeader />
+    </main>
+  );
+}
---
+++ src/app/Terms/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+import MainHeader from "../ui/header";
+
+export default function Terms() {
+  return (
+    <main>
+      <MainHeader />
+    </main>
+  );
+}
